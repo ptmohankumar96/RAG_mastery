422dbc17f0e27357a1b6d82f9e34a68affed4f10
ptmohankumar96 <ptmohankumar96@gmail.com>
2026-02-23 08:33:21 +0530

    Basic text search

---
+++ cli/keyword_search_cli.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Keyword Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    search_parser = subparsers.add_parser("search", help="Search movies using BM25")
+    search_parser.add_argument("query", type=str, help="Search query")
+
+    args = parser.parse_args()
+
+    with open("data/movies.json", "r", encoding="utf-8") as f:
+        data = json.load(f) 
+
+    match args.command:
+        case "search":
+            print(f'Searching for: {args.query}')
+            results = []
+            for movie in data["movies"]:
+                if args.query in movie["title"]:
+                    results.append(movie)
+            results.sort(key=lambda m: m["id"])
+            results = results[:5]
+            for i in range(len(results)):
+                print(f"{i+1}. {results[i]['title']}")
+
+            # More optimized one
+
+            # results = [m for m in data["movies"] if args.query in m["title"]]
+            # results.sort(key=lambda m: m["id"])
+            # for i, movie in enumerate(results[:5], start=1):
+            #     print(f"{i}. {movie['title']}")
+
+        case _:
+            parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
